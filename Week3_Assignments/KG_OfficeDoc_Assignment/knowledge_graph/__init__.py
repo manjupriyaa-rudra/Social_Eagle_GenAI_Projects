337422af8f69c07f19eb0f720b4ac64f43d98eaa
manjupriyaa-rudra <manjupriyaar.learning@gmail.com>
2026-02-08 21:15:24 +0530

    Delete Week3_Assignments/KG_OfficeDoc_Assignment directory

--- Week3_Assignments/KG_OfficeDoc_Assignment/knowledge_graph/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Knowledge Graph implementation using Graphiti and Neo4j."""
-
-from .kg_pipeline import KnowledgeGraphRAG
-from .query import query_kg
-
-__all__ = ['KnowledgeGraphRAG', 'query_kg']
--- Week3_Assignments/KG_OfficeDoc_Assignment/traditional_rag/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Traditional RAG implementation using LangChain and FAISS."""
-
-from .rag_pipeline import TraditionalRAG
-from .query import query_rag
-
-__all__ = ['TraditionalRAG', 'query_rag']
